fa70dfaf98162cb65b44f42befb49ad1864805ed
Marcela1204 <131552194+Marcela1204@users.noreply.github.com>
2023-10-29 13:52:22 +0000

    Funcao novo cliente

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include "library.h"
 
-Cadastro cliente[1000];
-ExtratoCliente grupo[1000][1000];
-int usados = 0;
+Cadastro cliente[1000];//struct de clientes
+ExtratoCliente grupo[1000][1000];//struct de extrato
+int usados;//variavel para percorrer os cliente
+
 
 int main() {
     int valor;
-
-    while (1){
-    printf("Digite o valor da operacao a ser realizada:\n");
+    while (1){//loop de menu
+    printf("Digite o valor da operacao a ser realizada:\n");//menu de destino
     printf("1- Novo Cliente;\n"
            "2- Apagar Cliente;\n"
            "3- Listar Clientes;\n"
@@ -19,41 +19,42 @@ int main() {
            "6- Extrato;\n"
            "7- Transferencia entre contas;\n"
            "0- Sair;\n");
-    printf("Qual operacao deseja realizar?\n");
-    scanf("%d", &valor);
+    printf("Qual operacao deseja realizar?\n");//pede o valor da operação
+    scanf("%d", &valor);//guarda o valor da operação
 
-    if (valor == 1) {
-        NovoCliente(&cliente,&usados, &grupo);
+    if (valor == 1) {//se o valor da operação for igual a 1
+        NovoCliente(&cliente,&usados, &grupo); //vai para função Novo cliente
     }
 
-    else if (valor == 2){
-        ApagarCliente(&cliente,&usados, &grupo);
+    else if (valor == 2){//se o valor da operação for igual a 2
+        
     }
 
-    else if (valor == 3){
-        ListarClientes(&cliente,&usados);
+    else if (valor == 3){//se o valor da operação for igual a 3
+        
     }
 
-    else if (valor == 4){
-        Debito(&cliente, &usados, &grupo);
+    else if (valor == 4){//se o valor da operação for igual a 4
+        
     }
 
-    else if (valor == 5){
-        Deposito(&cliente,&usados);
-
-    }
-
-    else if (valor == 6){
-        Extrato(&cliente,&usados, &grupo);
+    else if (valor == 5){//se o valor da operação for igual a 5
+        
 
     }
 
-    else if (valor == 7){
+    else if (valor == 6){//se o valor da operação for igual a 6
+        
 
     }
 
-    else if (valor == 0){
-        break;
+    else if (valor == 7){//se o valor da operação for igual a 7
+        
+
+    }
+
+    else if (valor == 0){//se o valor da operação for igual a 0
+        break;//o programa se encerra
     }
 
     }
