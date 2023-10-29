e3878b332d7adca3e331be5922aa4abe63c08105
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2023-10-29 13:45:46 +0000

    Versão 5

--- library.c
+++ library.c
@@ -5,7 +5,7 @@
 #include "library.h"
 #include <string.h>
 
-void NovoCliente (Cadastro *pessoas,int *usados){
+void NovoCliente (Cadastro *pessoas,int *usados, ExtratoCliente *grupo[]){
     while (1){
         char mensagem[4];
         printf("\nQuer continuar a operacao: (Sim ou Nao) R: \n");
@@ -48,6 +48,11 @@ void NovoCliente (Cadastro *pessoas,int *usados){
             if (comparacao == 0){
                 strcpy(pessoas[*usados].senha,senhatemporaria);
                 printf("Conta criada com sucesso!\n");
+                char tmpsaldo[500];
+                sprintf(tmpsaldo, "Conta criada.\n"
+                                  "Deposito : %.2lf", pessoas[*usados].saldo);
+                strcpy(grupo[*usados][0].info,tmpsaldo);
+                pessoas[*usados].transacao = 1;
                 *usados+=1;
             } else{
                 printf("Senha incorreta! Realize a operacao novamente!\n");
@@ -60,7 +65,7 @@ void NovoCliente (Cadastro *pessoas,int *usados){
 
 }
 
-void ApagarCliente (Cadastro *pessoas, int *usados){
+void ApagarCliente (Cadastro *pessoas, int *usados, ExtratoCliente *grupo){
     char cpf[11];
     printf("Digite o cpf a ser removido: ");
     scanf("%s",cpf);
@@ -103,7 +108,8 @@ void ApagarCliente (Cadastro *pessoas, int *usados){
     }else{
         for (int h = j; h < 999; ++h) {
             pessoas[h] = pessoas[h+1];
-            printf("%s",pessoas[h].nome);//temporario
+            grupo[h] = grupo[h+1];
+            //printf("%s",pessoas[h].nome);//temporario
         }
     }
     *usados-=1;
@@ -125,7 +131,7 @@ void ListarClientes (Cadastro *pessoas, int *usados){
 
 }
 
-void Debito (Cadastro *pessoas, int *usados){
+void Debito (Cadastro *pessoas, int *usados, ExtratoCliente *grupo[]){
     char cpf[11];
     while (1) {
         char mensagem[4];
@@ -145,7 +151,7 @@ void Debito (Cadastro *pessoas, int *usados){
                     printf("CPF nao encontrado!\n");
                     return;
                 }else{
-                    continue;//Isso funciona? O código chega nesse cara? SIM
+                    continue;
                 }
             }
             while (1){
@@ -166,11 +172,15 @@ void Debito (Cadastro *pessoas, int *usados){
             double valordigitado;
             printf("Quanto deseja debitar da conta? R: ");
             scanf("%lf",&valordigitado);
-            if (pessoas[*usados].minimo <= pessoas[*usados].saldo - (pessoas[*usados].taxa * valordigitado+valordigitado)){
-                pessoas[*usados].saldo = pessoas[*usados].saldo - (pessoas[*usados].taxa * valordigitado)-valordigitado;
-                printf("Seu saldo atual eh: %lf", pessoas[*usados].saldo);
+            if (pessoas[i].minimo <= pessoas[i].saldo - ((pessoas[i].taxa * valordigitado)+valordigitado)){
+                pessoas[i].saldo = pessoas[i].saldo - (pessoas[i].taxa * valordigitado)-valordigitado;
+                printf("Seu saldo atual eh: %.2lf", pessoas[i].saldo);
+                char tmpsaldo[500];
+                sprintf(tmpsaldo,"Debito : %.2lf", pessoas[i].saldo);
+                strcpy(grupo[i][pessoas[i].transacao].info,tmpsaldo);
+                pessoas[i].transacao++;
             } else{
-                printf("Valor do debito superior ao valor disponivel na conta!", pessoas[*usados].saldo);
+                printf("Valor do debito superior ao valor disponivel na conta!", pessoas[i].saldo);
             }
 
 
@@ -185,10 +195,20 @@ void Debito (Cadastro *pessoas, int *usados){
 void Deposito (Cadastro *pessoas, int *usados) {
     char cpf[11];
     int i;
-    printf("Digite seu cpf para deposito");
+    printf("Digite seu cpf para deposito: ");
     scanf("%s",cpf);
-    for (i = 0; i <= *usados + 1; ++i) {
-        int resultado = stricmp(cpf, pessoas[i].cpf);
+    //int exec = 1;
+    for (i = 0; i < *usados + 1; ++i) {
+        char comparativo[11];
+        strcpy(comparativo,pessoas[i].cpf);
+        int resultado = stricmp(cpf, comparativo);
+        //printf("exec %d\n",exec);
+        //exec++;
+       // for (int j = 0; j < 11; ++j) {
+            //printf("\n");
+           // int valor = cpf[j]-comparativo[j];
+           // printf("%d\n",valor);
+       // }
         if (resultado == 0) {
             break;
         } else if (resultado != 0 && i > *usados) {
@@ -199,12 +219,116 @@ void Deposito (Cadastro *pessoas, int *usados) {
         }
 
     }
-    printf("insira o valor a ser depositado");
+    printf("Insira o valor a ser depositado: \n");
     double valor;
     scanf("%lf",&valor);
-    pessoas[i].saldo+=valor;
+    pessoas[i].saldo = pessoas[i].saldo + valor;
+    printf("Seu saldo atual é %.2lf\n", pessoas[i].saldo);
 
 }
-void Extrato (Cadastro *) {}
-void Transfarencia (Cadastro *) {}
-void DebitoAutomatico (Cadastro *) {}
+void Extrato (Cadastro *pessoas ,int *usados, ExtratoCliente *grupo) {
+    char cpf[11];
+    while (1) {
+        char mensagem[4];
+        printf("\n");
+        printf("Quer continuar a operacao: (Sim ou Nao) R: \n");
+        scanf("%s", mensagem);
+        int positivo = stricmp(mensagem, "Sim");
+        if (positivo == 0) {
+            printf("Digite seu CPF: ");
+            scanf("%s", cpf);
+            int i;
+            for (i = 0; i <= *usados + 1; ++i) {
+                int resultado = stricmp(cpf, pessoas[i].cpf);
+                if (resultado == 0) {
+                    break;
+                } else if (resultado != 0 && i > *usados) {
+                    printf("CPF nao encontrado!\n");
+                    return;
+                } else {
+                    continue;//Isso funciona? O código chega nesse cara? SIM
+                }
+            }
+            while (1) {
+                char senhaconfirma[20];
+                strcpy(senhaconfirma, pessoas[i].senha);
+                char senhatemporaria[20];
+                printf("Digite sua senha: ");
+                scanf("%s", senhatemporaria);
+                int comparacao = stricmp(senhatemporaria, senhaconfirma);
+                if (comparacao == 0) {
+                    break;
+                } else {
+                    printf("Senha incorreta! Realize a operacao novamente!\n");
+                    continue;
+                }
+            }
+
+
+        }
+    }
+
+
+
+}
+
+
+void Transferencia (Cadastro *pessoas, int *usados){
+    char cpf[11];
+    char cpf2[11];
+    int i;
+    int j;
+    printf("Digite seu cpf de origem: ");
+    scanf("%s",cpf);
+    //int exec = 1;
+    for (i = 0; i < *usados + 1; ++i) {
+        char comparativo[11];
+        strcpy(comparativo, pessoas[i].cpf);
+        int resultado = stricmp(cpf, comparativo);
+        if (resultado == 0) {
+            break;
+        } else if (resultado != 0 && i > *usados) {
+            printf("CPF nao encontrado!\n");
+            return;
+        } else {
+            continue;
+        }
+    }
+    while (1){
+        char senhaconfirma[20];
+        strcpy(senhaconfirma,pessoas[i].senha);
+        char senhatemporaria[20];
+        printf("Digite sua senha: ");
+        scanf("%s", senhatemporaria);
+        int comparacao = stricmp(senhatemporaria,senhaconfirma);
+        if (comparacao == 0){
+            break;
+        } else {
+            printf("Senha incorreta! Realize a operacao novamente!\n");
+            continue;
+        }
+    }
+        printf("Digite seu cpf de destino: ");
+        scanf("%s",cpf2);
+        for (j = 0; j < *usados + 1; ++j) {
+            char comparativo[11];
+            strcpy(comparativo, pessoas[j].cpf);
+            int resultado = stricmp(cpf2, comparativo);
+            if (resultado == 0) {
+                break;
+            } else if (resultado != 0 && j > *usados) {
+                printf("CPF nao encontrado!\n");
+                return;
+            } else {
+                continue;
+            }
+        }
+        double transferencia;
+        printf("quanto deseja transferir?");
+        scanf("%lf", &transferencia);
+        pessoas[i].saldo-=transferencia;
+        pessoas[j].saldo+=transferencia;
+
+
+
+}
